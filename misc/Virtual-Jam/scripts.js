ef1544f1b5921c27815283fae93d82e3781d081a
Ari Hu <ariwho1@gmail.com>
2024-07-04 19:49:17 -0500

    Removed WebSocket functionality

--- misc/Virtual-Jam/scripts.js
+++ misc/Virtual-Jam/scripts.js
@@ -1,12 +1,12 @@
-const socket = io('ariservers.com', {transports: ['websocket'], secure: true});
+// const socket = io('ariservers.com', {transports: ['websocket'], secure: true});
 
-socket.on('connect', () =>{
-    console.log("Connected to backend");
-});
+// socket.on('connect', () =>{
+//     console.log("Connected to backend");
+// });
 
-socket.on('disconnect', () => {
-    console.log("Disconnected from backend");
-});
+// socket.on('disconnect', () => {
+//     console.log("Disconnected from backend");
+// });
 
 
 const keys = document.querySelectorAll('.piano-key');
@@ -23,8 +23,10 @@ keys.forEach((key) => {
         console.log("Pressed Piano Note: " + pressedKey);
         // Press Note -> Server Broadcasts -> Note is played
         //Uncomment below for Press Note -> Note is played -> ServerBroadcasts
-        // playSound(pressedKey, Instrument.Piano);
-        socket.emit('notePress', {note: pressedKey, instrument: Instrument.Piano});
+        playSound(pressedKey, Instrument.Piano);
+
+
+        // socket.emit('notePress', {note: pressedKey, instrument: Instrument.Piano});
     });
 });
 
@@ -32,15 +34,16 @@ drums.forEach((drum) => {
     drum.addEventListener('mousedown', (e) =>{
         const pressedDrum = drum.dataset.note;
         console.log("Pressed Drum Note: " + pressedDrum);
-        socket.emit('notePress', {note: pressedDrum, instrument: Instrument.Drums});
+        playSound(pressedDrum, Instrument.Piano);
+        // socket.emit('notePress', {note: pressedDrum, instrument: Instrument.Drums});
     });
 
 });
 
-socket.on('notePress', data => {
-    console.log("Received Note from server: " + data.note + ", " + data.instrument);
-    playSound(data.note, data.instrument);
-})
+// socket.on('notePress', data => {
+//     console.log("Received Note from server: " + data.note + ", " + data.instrument);
+//     playSound(data.note, data.instrument);
+// })
 
 function playSound(note, instrument){
     console.log("Play Note Sound");
