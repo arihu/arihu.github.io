29fbe219bf1ce4bf89f4212a5f4f86587a8d87dd
Ari Hu <ariwho1@gmail.com>
2024-07-08 19:22:15 -0500

    fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,18 @@
 function cubePassword() {
     var password = prompt("Please enter the password:");
-    if (validatePassword(password)) {
+
+    if (password != null && validatePassword(password)) {
         window.location.href = "/misc/Cube-World/";
-    } else if (password !== null) {
+    } else {
         alert("Incorrect password. Please try again.");
     }
 }
 
 //Can you crack it?
 function validatePassword(pw){
+    if(pw === null)
+        return false;
+    pw += "f92j1028";
     let c0 = String.fromCharCode(pw[0].charCodeAt(0) - 22);
     let c1 = String.fromCharCode(pw[1].charCodeAt(0) - 33);
     let c2 = String.fromCharCode(pw[2].charCodeAt(0) - 25);
@@ -18,8 +22,7 @@ function validatePassword(pw){
     let c6 = String.fromCharCode(pw[6].charCodeAt(0) - 50);
     let c7 = String.fromCharCode(pw[7].charCodeAt(0) - 49);
     let pass = c0 + c1 + c2 + c3 + c4 + c5 + c6 + c7;
-    console.log(pass);
-    console.log("btoa: " + btoa("1234"));
-    
+    if(pass === btoa("1234"))
+        return true;
     return pass === btoa("1234");
 }
